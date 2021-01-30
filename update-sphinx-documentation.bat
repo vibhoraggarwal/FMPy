c1bb80a23d260b055bb279acc9b76144dc126150
Vibhor Aggarwal <vibhorag24@gmail.com>
2021-01-30 03:24:49 +0100

    Updating the documentation

--- update-sphinx-documentation.bat
+++ update-sphinx-documentation.bat
@@ -1,6 +1,7 @@
 git pull
 sphinx-apidoc -o sphinx/source -f fmpy --ext-autodoc
 make html
+copy sphinx/build/html/* docs/html
 git add .
 git commit -m "Updating the documentation"
 git push origin master
